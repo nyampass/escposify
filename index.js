9549cbc67b03886b673c8b0a3ce1ac8aa69199e2
OHTA Shogo <athos0220@gmail.com>
2016-12-27 15:38:15 +0900

    Remove USB-related deps, which are unnecessary for my usage

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ exports.Printer = exports.printer = require('./lib/printer');
 exports.image = require('./lib/image');
 exports.qrimage = require('./lib/qrimage');
 
-exports.USB = require('./lib/connectors/usb');
 exports.Console = require('./lib/connectors/console');
